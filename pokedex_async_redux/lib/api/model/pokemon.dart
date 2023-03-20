2c2a1187e3307ee6f2a8f893278f5457b10a23b2
zrnkrystnlee <2193894@slu.edu.ph>
2023-03-20 18:02:03 +0800

    feat: added the pokemon list model - added the pokemon.dart inside model folder containing the name and url of the pokemon and it was also freezed - added the function getPokemonList in pokemon_api.dart - debugged and showed the 151 pokemons - added a TODO Remove in the main.dart - remove the @Default in pokemon.dart

--- pokedex_async_redux/lib/api/model/pokemon.dart
+++ pokedex_async_redux/lib/api/model/pokemon.dart
@@ -7,8 +7,8 @@ part 'pokemon.g.dart';
 @freezed
 class Pokemon with _$Pokemon {
   factory Pokemon({
-    @Default('') @JsonKey(name: 'name') String name,
-    @Default('') @JsonKey(name: 'url') String url,
+    @JsonKey(name: 'name') String name,
+    @JsonKey(name: 'url') String url,
   }) = _Pokemon;
 
   factory Pokemon.fromJson(Map<String, dynamic> json) => _$PokemonFromJson(json);
